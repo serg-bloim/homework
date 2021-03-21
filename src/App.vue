037011add94a5cae1c0b6f1e54957aea75a386e0
Sergey Bilonozhko <serhii_bilongozhko@comcast.com>
2021-03-21 03:31:10 -0400

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <b-button>Test button</b-button>
+    <arythmetics-task :problem="'2+2'" :answer="4"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import ArythmeticsTask from './components/ArythmeticsTask.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ArythmeticsTask
   }
 }
 </script>
---
+++ src/components/ArythmeticsTask.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ problem }} = {{ answer }}</div>
+</template>
+
+<script>
+export default {
+  name: "ArythmeticsTask",
+  props: {
+    problem: String,
+    answer: String,
+  },
+};
+</script>
