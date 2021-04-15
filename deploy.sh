5cf1e057a77127910171a224dfe838246c740248
Sergey Bilonozhko <serhii_bilongozhko@comcast.com>
2021-04-14 23:15:31 -0400

    upd

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
+latest_v=$(git describe --abbrev=0 --tags)
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "Latest version is $latest_v"
+echo "Enter new version"
+read new_v
+git tag "$new_v"
 npm install
 npm run build
 git checkout site
 rm -rf docs
 mv dist docs
 git add docs
-git commit
-git checkout master
+git commit -m "Releasing $new_v"
+git checkout "$current_branch"
