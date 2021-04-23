b39fd60639099a7ae7068a5e8601131807260930
Sergey Bilonozhko <serhii_bilongozhko@comcast.com>
2021-04-23 18:07:38 -0400

    serve static data from /data/ directory

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
     publicPath: "/homework",
+    devServer:{
+        contentBase: path.join(__dirname, '../data')
+    }
 }
